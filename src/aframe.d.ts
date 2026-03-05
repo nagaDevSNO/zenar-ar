5fb5a287fffc0ac65fa872b3c348f28146a5279c
Nagaraju Yellanki <nagadevsn@gmail.com>
2026-03-05 14:56:19 -0600

    initial ZenAR project

--- src/aframe.d.ts
+++ src/aframe.d.ts
@@ -1,20 +1,19 @@
-export {};
+export {}
 
 declare global {
   namespace JSX {
     interface IntrinsicElements {
-      "a-scene": any;
-      "a-marker": any;
-      "a-entity": any;
-      "a-assets": any;
-      "a-plane": any;
-      "a-text": any;
-      "a-cylinder": any;
-      "a-sphere": any;
-      "a-box": any;
-      "a-camera": any;
-      "a-light": any;
-      [elemName: string]: any;
+      "a-scene": any
+      "a-marker": any
+      "a-entity": any
+      "a-assets": any
+      "a-plane": any
+      "a-text": any
+      "a-cylinder": any
+      "a-sphere": any
+      "a-box": any
+      "a-camera": any
+      "a-light": any
     }
   }
 }
